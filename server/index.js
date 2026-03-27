4e35dec2ecdbea359bd0f2da2eaa23d9a1ac34f2
Nkhoa-BE <khoa.2374802010234@vanlanguni.vn>
2026-03-27 12:55:03 +0700

    Refactor server/index.js for improved structure

--- server/index.js
+++ server/index.js
@@ -1,32 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
+const bodyParser = require('body-parser');
 
-// middlewares
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
-// apis
+// API
 app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+    res.json({ message: 'Hello from server!' });
 });
+
 app.use('/api/admin', require('./api/admin.js'));
 app.use('/api/customer', require('./api/customer.js'));
 
+// ADMIN FRONTEND
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('/admin/:path(*)', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build-admin/index.html'));
+
+app.get(/^\/admin\/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
 });
+
+// CUSTOMER FRONTEND
 app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
-app.get('*', (req, res) => {
+
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
 });
 
-// start server
+// START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-})
+    console.log(`Server listening on ${PORT}`);
+});
