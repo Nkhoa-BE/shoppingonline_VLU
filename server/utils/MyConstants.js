188c87189c9e896f0252f6b3cb998e81c5d45847
Nkhoa-BE <khoa.2374802010234@vanlanguni.vn>
2026-03-27 11:05:34 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.clfkzwp.mongodb.net',
+  DB_USER: 'khoa',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+
+  EMAIL_USER: 'nguyenkhoa21102005@gmail.com', 
+  EMAIL_PASS: 'dncrveizlyqqtmvr',
+
+  JWT_SECRET: 'abc',
+  JWT_EXPIRES: '1000000000000', 
+};
+
+module.exports = MyConstants;
